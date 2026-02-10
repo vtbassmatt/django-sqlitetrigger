b0290d4abe968d269fdffe56b14e3cb997ceb2e1
Matt Cooper <vtbassmatt@gmail.com>
2026-02-10 09:52:31 -0500

    manually-installable trigger, but makemigrations isn't working

--- example/example/models.py
+++ example/example/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+from sqlitetrigger import Trigger, After, Update
+
 # Create your models here.
 class Book(models.Model):
+    class Meta:
+        triggers = [
+            Trigger(
+                name='book_sheets_update',
+                when=After,
+                operation=Update,
+                func="UPDATE example_book SET sheets = new.pages / 2 WHERE id = new.id;",
+            )
+        ]
     title = models.CharField(max_length=100)
     pages = models.PositiveIntegerField()
     sheets = models.PositiveIntegerField(editable=False)
